99d70a23234f3c3d1fd0d4feb8c4d8b08896bddb
Ratneshwar <ratneshwar.jha@hcl.com>
2022-05-13 08:13:23 +0530

    Initial commit

---
+++ UnitTestProject1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HCL")]
+[assembly: AssemblyProduct("UnitTestProject1")]
+[assembly: AssemblyCopyright("Copyright © HCL 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c650f731-7a3e-4e1f-8205-fdb179f19fed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String str = "Hello";
+            Console.WriteLine(str);
+            // var driver = new ChromeDriver();
+            //var driver =  GetChromeDriver();
+            //driver.Navigate().GoToUrl("https://ultimateqa.com/");
+
+            //If ChromeDrive is in project itself and is attached 
+            IWebDriver driver= new ChromeDriver();
+
+            //IWebDriver driver = new ChromeDriver("C:\\Users\\ratneshwar.jha\\Downloads\\chromedriver_win32");
+            driver.Navigate().GoToUrl("https://ultimateqa.com/");
+            Assert.IsTrue(true);
+            Console.WriteLine(driver.Title);
+            Assert.AreEqual("Home | Ultimate QA", driver.Title);
+            
+        }
+
+        /*private IWebDriver GetChromeDriver()
+        {
+            var outPutDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            return new ChromeDriver(outPutDirectory);
+        }*/
+    }
+}
